88f16a12687de965df283fd363b0143c593b6889
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-06-04 16:34:15 -0600

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -1,10 +1,8 @@
-##### - Retrieve all the information from the facilities table.
-```sql
+-- Retrieve all the information from the facilities table.
 SELECT *
 FROM cd.facilities;
-```
-##### - Print out a list of all of the facilities and their cost to members.
-```sql
+
+-- Print out a list of all of the facilities and their cost to members.
 SELECT name, membercost
 FROM cd.facilities;
-```
+
