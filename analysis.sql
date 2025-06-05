00af20ceea923ed644e5a19590c1759a47ba3ba3
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-06-05 07:13:10 -0600

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -1,12 +1,17 @@
--- Retrieve all the information from the facilities table.
+-- Retrieve all the information from the facilities table:
 SELECT *
 FROM cd.facilities;
 
---- Retrieve a list of only facility names and cost of usage.
+--- Retrieve a list of only facility names and cost of usage:
 SELECT name, membercost, guestcost
 FROM cd.facilities;
 
---- Retrieve a list of facilities that charge a fee to members, and that fee is less than 1/50th of the monthly maintenance cost?
+--- Retrieve a list of only facilities that charge a fee to members:
+SELECT name, membercost
+FROM cd.facilities
+WHERE membercost > 0;
+
+--- Retrieve a list of facilities that charge a fee to members, and that fee is less than 1/50th of the monthly maintenance cost:
 SELECT facid, name, membercost, monthlymaintenance
 FROM cd.facilities
 WHERE membercost > 0 AND membercost < (monthlymaintenance/50);
