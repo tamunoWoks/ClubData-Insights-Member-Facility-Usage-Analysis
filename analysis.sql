5c72f46205894e83e0415d1b63ddd9d9482b4cb1
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-06-05 07:03:00 -0600

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -6,3 +6,7 @@ FROM cd.facilities;
 SELECT name, membercost, guestcost
 FROM cd.facilities;
 
+--- Retrieve a list of facilities that charge a fee to members, and that fee is less than 1/50th of the monthly maintenance cost?
+SELECT facid, name, membercost, monthlymaintenance
+FROM cd.facilities
+WHERE membercost > 0 AND membercost < (monthlymaintenance/50);
