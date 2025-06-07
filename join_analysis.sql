e731659f57cab751d9dda885f9f765457f908cbd
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-06-07 16:34:45 -0600

    Update join_analysis.sql

--- join_analysis.sql
+++ join_analysis.sql
@@ -1,8 +1,8 @@
 --- Retrieve a list of the start times for bookings by 'David Farrell':
 SELECT 
-	(m.firstname||' '||m.surname) as member,
-	b.starttime
-FROM cd.members as m
-JOIN cd.bookings as b 
-	ON m.memid = b.memid
-WHERE m.firstname = 'David' AND m.surname = 'Farrell';
+	(mem.firstname||' '||mem.surname) as member,
+	bks.starttime
+FROM cd.members as mem
+JOIN cd.bookings as bks 
+	ON mem.memid = mem.memid
+WHERE mem.firstname = 'David' AND mem.surname = 'Farrell';
