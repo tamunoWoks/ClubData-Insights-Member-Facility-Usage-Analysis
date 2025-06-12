06a83197ea315d79ed38b9490653e38e0d1886d7
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-06-12 07:58:41 -0600

    Update join_analysis.sql

--- join_analysis.sql
+++ join_analysis.sql
@@ -40,3 +40,11 @@ ORDER BY 2, 1;
 
 --- Retrieve a list of all members, along with their recommender.
 --- Ensure that results are ordered by (surname, firstname).
+SELECT mem.firstname AS memfname,
+	mem.surname AS memsname,
+	rec.firstname AS recfname,
+	rec.surname AS recsname
+FROM cd.members mem
+LEFT JOIN cd.members rec
+	ON rec.memid = mem.recommendedby
+ORDER BY 2, 1;
